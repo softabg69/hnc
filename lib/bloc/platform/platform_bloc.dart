9eb80aacbe4a59159e243de1488e3e85e393563c
abg <softabg@gmail.com>
2022-05-29 11:05:35 +0200

    nueva versión

--- lib/bloc/platform/platform_bloc.dart
+++ lib/bloc/platform/platform_bloc.dart
@@ -4,6 +4,8 @@ import 'package:helpncare/enumerados.dart';
 import 'package:helpncare/repository/hnc_repository.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
+import '../../components/log.dart';
+
 part 'platform_event.dart';
 part 'platform_state.dart';
 
@@ -31,11 +33,13 @@ class PlatformBloc extends Bloc<PlatformEvent, PlatformState> {
             buildSignature: info.buildSignature,
             resultadoVersion: ResultadoComparaVersion.iguales));
       } else {
+        Log.registra('version: ${version.version}');
+        Log.registra('info: ${info.version}');
         final vApp = version.version.split('.');
         final vBD = info.version.split('.');
         final nvApp = int.parse(vApp[2]);
         final nvBD = int.parse(vBD[2]);
-        if (vApp[0] == vBD[0] && vApp[1] == vBD[1] && nvBD > nvApp) {
+        if (vApp[0] == vBD[0] && vApp[1] == vBD[1] && nvApp > nvBD) {
           emit(PlatformState(
               estado: EstadoPlatform.disponible,
               appName: info.appName,
